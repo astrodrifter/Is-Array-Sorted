9f1d999eeaba141c7ed74db65f8d8cf9d82ad923
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-17 21:59:54 +1000

    Not working
    An attempt to solve it. If not ascending return false and if not
    descending return false else return true.

---
+++ IsArraySorted/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsArraySorted {
+    class Program {
+        static void Main(string[] args) {
+            int[] numbers = { 5, 4, 3, 2, 1 };
+            bool result = IsArraySorted(numbers);
+            DisplayResult(result);
+            Console.ReadKey();
+
+
+        }
+        public static bool IsArraySorted(int[] numbers) {
+            int length = numbers.Length;
+            for(int i = 0; i < length; i++) {
+                if (numbers[i] < numbers[i+1] && numbers[i] > numbers[i+2]) {
+                    Console.WriteLine("numbers is not sorted #1");
+                    return false;
+                } else if (numbers[i] > numbers[i+1] && numbers[i] < numbers[i+2]) {
+                    Console.WriteLine("numbers is not sorted #2");
+                    return false;
+                }              
+            }
+            return true;
+        }
+        public static bool DisplayResult(bool result) {
+            if(result == true) {
+                Console.WriteLine("The array is sorted");
+            }
+            return true;
+        }
+    }
+}
